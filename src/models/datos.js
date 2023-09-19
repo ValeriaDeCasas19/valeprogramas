ab4e9660aa0324a9c6caf737a2b8cc31a7eca99f
ValeriaDeCasas19 <valeriadecasasa@gmail.com>
2023-09-18 19:55:22 -0600

    conexion mongodb y express

--- src/models/datos.js
+++ src/models/datos.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Producto = new Schema({
+const Datos = new Schema({
     producto: String,
     precio: String,
     iva: String
 });
 
-module.exports = mongoose.model('datos', Producto);
+module.exports = mongoose.model('datos', Datos);
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const model = require('../models/productos.js')();
-const Producto = require('../model/productos.js');
+const model = require('../models/datos.js')();
+const Datos = require('../models/datos.js');
 
 router.get('/', async (req, res) =>{
-    const datos = await Producto.find();
+    const datos = await Datos.find();
     console.log(datos);
     res.render('index.ejs',{
         datos
     });
 
-}); //get obtiene y post manda datos
+});
+router.post('/add', async(req,res)=>{
+    const valor = new Datos(req.body);
+    await valor.save();
+    res.redirect('/');
+});
+
+router.get('/del/:id', async(req,res)=>{
+    const {id}=req.params;
+    await Datos.findByIdAndRemove(id);
+    res.redirect('/');
+})
+
+//get obtiene y post manda datos
 module.exports = router;
+//tarea: que al eliminar abra una ventana modal para que se confirme la eiminacion 
