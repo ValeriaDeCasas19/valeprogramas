29b0e539108f7a1c6727bbfa5e05b86191fdd26c
ValeriaDeCasas19 <valeriadecasasa@gmail.com>
2023-09-23 21:21:16 -0600

    creacion del boton actualizar

--- src/routers/index.js
+++ src/routers/index.js
@@ -11,20 +11,39 @@ router.get('/', async (req, res) =>{
     });
 
 });
-router.post('/add', async(req,res)=>{
+router.post('/add', async(req,res)=>{ //para agregar
     const valor = new Datos(req.body);
     await valor.save();
     res.redirect('/');
 });
 
-router.get('/del/:id', async(req,res)=>{
+router.get('/del/:id', async(req,res)=>{ //para eliminar
     const {id}=req.params;
     await Datos.findByIdAndRemove(id);
     res.redirect('/');
-})
+});
+
+// router.get('/upd/:id', async(req,res)=>{ //para actualizar
+//     const {id}=req.params;
+//     await Datos.findByIdAndUpdate(id);
+//     res.redirect('/');
+// });
+
+router.post('/upd/:id', async(req, res)=>{
+    const{id} = req.params;
+    const{producto, precio, iva} = req.body;
+    await Datos.findByIdAndUpdate(id, {producto, precio, iva});
+    res.redirect('/')
+});
+
+
+
+
 
 
 
 //get obtiene y post manda datos
 module.exports = router;
-//tarea: que al eliminar abra una ventana modal para que se confirme la eiminacion 
+//tarea: agregar un boton al lado del eliminar y al de registrar para que una vez que se presione al lado de 
+//eliminar se pongan los datos del registro seleccionado en los campos de arriba, se puedan editar y al darle 
+//clic en actualizar al lado de registrar se actualicen los valores en la tabla express. 
